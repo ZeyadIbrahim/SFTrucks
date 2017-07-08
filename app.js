b0192fe2bfee2517de8cbe2a024dc37fd43a826f
johnnyboyy <zdanis.john@gmail.com>
2017-07-07 23:23:59 -0700

    cleanup and convert to pug for view engine

--- app.js
+++ app.js
@@ -1,32 +1,66 @@
-// get the http module:
-var http = require('http');
-// fs module allows us to read and write content for responses!!
-var fs = require('fs');
-// creating a server using http module:
-var server = http.createServer(function (request, response){
-    // see what URL the clients are requesting:
-    console.log('client request URL: ', request.url);
-    // this is how we do routing:
-    if(request.url === '/') {
-        fs.readFile('index.html', 'utf8', function (errors, contents){
-            response.writeHead(200, {'Content-Type': 'text/html'});
-            response.write(contents); 
-            response.end();
-        });
-    }
-    else if (request.url === "/dojo.html") {
-         fs.readFile('dojo.html', 'utf8', function (errors, contents){
-             response.writeHead(200, {'Content-type': 'text/html'});
-             response.write(contents); 
-             response.end();
-         });
-    }
-    // request didn't match anything:
-    else {
-        response.end('File not found!!!');
-    }
+
+// Remove after catching Zeyad up on how to start the server
+var npm_start_used = process.env.JZ || false;
+
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var index = require('./routes/index');
+var map_route = require('./routes/map');
+
+// create the express app
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+
+
+
+app.use('/', index);
+app.use('/map', map_route);
+
+
+
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
-// tell your server which port to run on
-server.listen(9000);
-// print to terminal window
-console.log("Running in localhost at port 9000");
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+if (npm_start_used === false) {
+  // tell the express app to listen on port 8000
+  app.listen(8000, function() {
+    console.log("listening on port 8000");
+  });
+}
+
+module.exports = app;
