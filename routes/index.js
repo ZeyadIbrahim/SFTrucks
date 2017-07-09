353f47326b8d81cce73ebe547a68aca36de35714
Johnnyboyy <zdanis.john@gmail.com>
2017-07-08 19:33:00 -0700

    start to make sense of api

--- routes/index.js
+++ routes/index.js
@@ -1,57 +1,52 @@
 var express = require('express');
 var router = express.Router();
+var request = require('request');
+var rp = require('request-promise');
 
 /* get map data. */
 router.get('/', function(req, res, next){
-  dummy = {
-    "data": [{
-      "type": "articles",
-      "id": "1",
-      "attributes": {
-        "title": "JSON API paints my bikeshed!",
-        "body": "The shortest article. Ever.",
-        "created": "2015-05-22T14:56:29.000Z",
-        "updated": "2015-05-22T14:56:28.000Z"
-      },
-      "relationships": {
-        "author": {
-          "data": {"id": "42", "type": "people"}
-        }
+
+  structureData = function(data) {
+    var cols = [
+      'sid', 'id', 'position', 'created_at', 'created_meta', 'updated_at',
+      'updated_meta', 'meta', 'DayOrder', 'DayOfWeekStr', 'starttime',
+      'endtime', 'permit', 'PermitLocation', 'locationdesc', 'optionaltext',
+      'locationid', 'scheduleid', 'start24', 'end24', 'CNN', 'Addr_Date_Create',
+      'Addr_Date_Modified', 'block', 'lot', 'ColdTruck', 'Applicant', 'X', 'Y',
+      'Latitude', 'Longitude', 'Location'
+    ];
+    var structured = [];
+
+    for (var i = 0; i < data.data.length; i++) {
+      structured[i] = {};
+      for (var j = 0; j < cols.length; j++) {
+        structured[i][cols[j]] = data.data[i][j];
       }
-    },{
-      "type": "articles",
-      "id": "2",
-      "attributes": {
-        "title": "JSON API paints my bikeshed!",
-        "body": "The shortest article. Ever.",
-        "created": "2015-05-22T14:56:29.000Z",
-        "updated": "2015-05-22T14:56:28.000Z"
-      },
-      "relationships": {
-        "author": {
-          "data": {"id": "42", "type": "people"}
-        }
-      }
-    }],
-    "included": [
-      {
-        "type": "people",
-        "id": "42",
-        "attributes": {
-          "name": "John",
-          "age": 80,
-          "gender": "male"
-        }
-      }
-    ]
+    }
+    return structured;
   }
-  res.apiData = dummy;
-  next();
+
+  var options = {
+    uri: 'https://data.sfgov.org/api/views/jjew-r69b/rows.json',
+    qs: {},
+    headers: { 'User-Agent': 'Request-Promise' },
+    json: true // Automatically parses the JSON string in the response
+  };
+
+  rp(options)
+    .then(function (json) {
+
+      res.apiData = structureData(json);
+      next();
+    })
+    .catch(function (err) {
+      res.send('Error');
+    });
 });
 
 /* Render home page. */
-router.get('/', function(req, res, next) {
-  res.render('map', { apiData: res.apiData });
+router.get('/', function(req, res) {
+  res.render('index', { apiData: res.apiData });
 });
 
 
